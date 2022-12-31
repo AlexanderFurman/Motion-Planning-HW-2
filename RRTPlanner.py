67f73bdedcd286664965ad489d24ee80c4b50484
tomagami1990 <58557948+tomagami1990@users.noreply.github.com>
2022-12-31 14:24:51 +0200

    changes2

--- RRTPlanner.py
+++ RRTPlanner.py
@@ -2,7 +2,7 @@ import numpy as np
 from RRTTree import RRTTree
 import time
 from MapEnvironment import MapEnvironment
-
+from collections import deque
 class RRTPlanner(object):
 
     def __init__(self, planning_env, ext_mode, goal_prob):
@@ -36,7 +36,7 @@ class RRTPlanner(object):
         x_range=self.planning_env.xlimit
         y_range = self.planning_env.ylimit
         #sample state:
-        for i in range(200):
+        for i in range(300): #number of iterations
             if (np.random.uniform(0, 1) < self.goal_prob): #bias goal
                 #take target as sample
                 a=self.planning_env.goal[0]
@@ -45,28 +45,21 @@ class RRTPlanner(object):
                 a=np.random.uniform(x_range[0], x_range[1])
                 b= np.random.uniform(y_range[0], y_range[1])
             new_state=np.array([a,b])
-            #ans=MapEnvironment.state_validity_checker(self.planning_env,new_state)
             x_near=self.tree.get_nearest_state(new_state)
             x_ext=self.extend(x_near[1],new_state)
-            ans = True#MapEnvironment.state_validity_checker(self.planning_env, x_ext)
-            if ans:
-
-                ans2=MapEnvironment.edge_validity_checker(self.planning_env,x_near[1],x_ext)
-                if ans2:
-                    self.tree.add_vertex(x_ext)
-                    x_new = self.tree.get_nearest_state(x_ext)
-                    edge_cost=self.compute_cost([x_near[1],x_ext])
-                    self.tree.add_edge(x_near[0],x_new[0],edge_cost)
+            if x_near[1][0] <0:
+                a=88
+            ans2=MapEnvironment.edge_validity_checker(self.planning_env,x_near[1],x_ext)
+            if ans2:
+                x_new_idx=self.tree.add_vertex(x_ext)
+                edge_cost=self.compute_cost([x_near[1],x_ext])
+                self.tree.add_edge(x_near[0],x_new_idx,edge_cost)
 
         #calculate plan
         if self.tree.is_goal_exists(self.planning_env.goal):
             print('goal exist')
-            #path = self.dijkstra()
-            #print(path)
+            plan = self.dijkstra()
 
-
-
-        
         # print total path cost and time
         print('Total cost of path: {:.2f}'.format(self.compute_cost(plan)))
         print('Total time: {:.2f}'.format(time.time()-start_time))
@@ -75,13 +68,40 @@ class RRTPlanner(object):
 
     def dijkstra(self):
         '''
-        shortest path
+        shortest path in the tree
         '''
         srcIdx = self.tree.get_root_id()
-        dstIdx = self.tree.get_nearest_state(self.planning_env.goal)
+        dstIdx = self.tree.get_nearest_state(self.planning_env.goal)[0]
 
         # build dijkstra
-        edges=self.tree.get_edges_as_states()
+        #edges = self.tree.get_edges_as_states()
+        edges=self.tree.edges
+        nodes = self.tree.vertices# list(G.neighbors.keys())
+        nodes_list=list(nodes.keys())
+        dist = {node: float('inf') for node in nodes_list}
+        prev = {node: None for node in nodes_list}
+        dist[srcIdx] = 0
+
+        while nodes_list:
+            curNode = min(nodes_list, key=lambda node: dist[node])
+            nodes_list.remove(curNode)
+            if dist[curNode] == float('inf'):
+                break
+
+            neighbors = [key for key, value in edges.items() if value == curNode ]
+            for neighbor in neighbors:
+                newCost = dist[curNode] + nodes[neighbor].cost
+                if newCost < dist[neighbor]:
+                    dist[neighbor] = newCost
+                    prev[neighbor] = curNode
+
+        #path
+        path = deque()
+        curNode = dstIdx
+        while prev[curNode] is not None:
+            path.appendleft(nodes[curNode].state)
+            curNode = prev[curNode]
+        path.appendleft(nodes[curNode].state)
         return list(path)
 
     def compute_cost(self, plan):
@@ -110,7 +130,8 @@ class RRTPlanner(object):
         length = np.linalg.norm(step_dir)
         #if new state enough close , leave it.
         step_dir = (step_dir / length) * min(step_size, length)
-        return((near_state[0] + step_dir[0], near_state[1] + step_dir[1]))
+        ext_state=np.asarray([near_state[0] + step_dir[0], near_state[1] + step_dir[1]])
+        return ext_state
 
 
 
